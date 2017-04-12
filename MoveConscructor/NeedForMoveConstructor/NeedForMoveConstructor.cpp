11534c8cbabcf106afb8761964497e87b1df4757
Amandeep Chawla <amandeep@adobe.com>
2017-04-12 09:47:49 +0530

    With all the logging

--- MoveConscructor/NeedForMoveConstructor/NeedForMoveConstructor.cpp
+++ MoveConscructor/NeedForMoveConstructor/NeedForMoveConstructor.cpp
@@ -5,31 +5,31 @@ class A {
 public:
     A(const std::string & name)
         : mName(name) {
-        //std::cout << "A's constructor for "<< mName.c_str() <<" object\n";
+        std::cout << "A's constructor for "<< mName.c_str() <<" object\n";
     }
 
     ~A() { 
-        //std::cout << "A's destructor for " << mName.c_str() << " object\n";
+        std::cout << "A's destructor for " << mName.c_str() << " object\n";
     }
 
     A(const A & src)
         : mName(src.mName) {
         mName.append(" - copy");
-        //std::cout << "A's copy constructor for " << mName.c_str() << " object\n";
+        std::cout << "A's copy constructor for " << mName.c_str() << " object\n";
     }
 
     A(const A && src)
         : mName(std::move(src.mName)) {
-        //std::cout << " A's move constructor for " << mName.c_str() << " object\n";
+        std::cout << "A's move constructor for " << mName.c_str() << " object\n";
     }
 
     A & operator = (const A & lhs) {
         if (this != &lhs) {
             mName = lhs.mName + " - copy";
-            //std::cout << "A's assignment operator for " << mName.c_str() << " object\n";
+            std::cout << "A's assignment operator for " << mName.c_str() << " object\n";
         }
         else {
-            //std::cout << "Nothing to be done in assignment operator for " << mName.c_str() << " object\n";
+            std::cout << "Nothing to be done in assignment operator for " << mName.c_str() << " object\n";
         }
     }
 
