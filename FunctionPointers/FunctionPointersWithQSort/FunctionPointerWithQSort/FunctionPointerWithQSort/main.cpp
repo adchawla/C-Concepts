fa619a0467ca48c5595e3838b404a5cbae668ad5
Amandeep Chawla <amandeep@adobe.com>
2017-04-19 00:24:18 +0530

    Adding descending sorting on characters

--- FunctionPointers/FunctionPointersWithQSort/FunctionPointerWithQSort/FunctionPointerWithQSort/main.cpp
+++ FunctionPointers/FunctionPointersWithQSort/FunctionPointerWithQSort/FunctionPointerWithQSort/main.cpp
@@ -19,6 +19,18 @@ int int_sorter( const void *first_arg, const void *second_arg )
     }
 }
 
+int char_sorter_desc(const void * first_arg, const void * second_arg) {
+    char first = *(char*)first_arg;
+    char second = *(char*)second_arg;
+    if (first < second) {
+        return 1;
+    } else if (first == second) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
 int main()
 {
     int array[10];
@@ -34,5 +46,8 @@ int main()
         printf ( "%d\n" ,array[ i ] );
     }
     
+    char charArray[] = "abcdefghijklmnopqrstuvwxyz";
+    qsort(charArray, sizeof(charArray) /sizeof(char) - 1, sizeof(char), char_sorter_desc);
+    printf("%s\n", charArray);
     
 }
