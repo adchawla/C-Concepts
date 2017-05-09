2d4e0f96b2bb6d0fc1bb7e97139883205ce59182
Amandeep Chawla <amandeep@adobe.com>
2017-05-09 09:52:52 +0530

    Driver program to use Nodes

--- SmartPointers/SmartPointers/Driver.cpp
+++ SmartPointers/SmartPointers/Driver.cpp
@@ -1,8 +1,42 @@
 #include "MyObject.h"
+#include "Node.h"
 #include <iostream>
+#include <vector>
+
+void printTree(Node * rootNode);
 
 int main() {
-    MyObject myObj1("1");
+    /*MyObject myObj1("1");
     MyObject * myObj2 = new MyObject("2");
-    delete myObj2;
+    std::vector<MyObject *> colletion;
+    colletion.push_back(&myObj1);
+    colletion.push_back(myObj2);
+    delete myObj2;*/
+    Node * rootNode = new Node("root");
+    Node * child1 = new Node("child1");
+    rootNode->addChild(child1);
+    rootNode->addChild(new Node("child2"));
+    rootNode->addChild(new Node("child3"));
+    rootNode->addChild(new Node("child4"));
+    child1->addChild(new Node("grandChild1"));
+    child1->addChild(new Node("grandChild2"));
+    child1->addChild(new Node("grandChild3"));
+    printTree(rootNode);
+    delete rootNode;
+    printTree(child1);
+}
+
+void  printTree(Node * node, size_t level) {
+    for (size_t i = 0; i < level; ++i) {
+        std::cout << "\t";
+    }
+    std::cout << node->id()<<"\n";
+    size_t childCount = node->countOfChildren();
+    for (size_t i = 0; i < childCount; ++i) {
+        printTree(node->getChild(i), level + 1);
+    }
+}
+
+void printTree(Node * rootNode) {
+    printTree(rootNode, 0);
 }
